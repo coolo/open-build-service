bb242915770bb66095a39b5483a73bb88b5569c7
Stephan Kulow <coolo@suse.de>
2019-05-19 12:24:40 +0200

    Reload staging workflow before comparing staging projects

--- src/api/spec/controllers/webui/staging/projects_controller_spec.rb
+++ src/api/spec/controllers/webui/staging/projects_controller_spec.rb
@@ -39,7 +39,7 @@ RSpec.describe Webui::Staging::ProjectsController do
       subject { staging_workflow }
 
       it { expect(Project.count).to eq(4) }
-      it { expect(subject.staging_projects.map(&:name)).to match_array(['home:tom:Staging:A', 'home:tom:Staging:B', existent_project.name]) }
+      it { expect(subject.reload.staging_projects.map(&:name)).to match_array(['home:tom:Staging:A', 'home:tom:Staging:B', existent_project.name]) }
       it { expect(response).to redirect_to(edit_staging_workflow_path(subject)) }
       it { expect(flash[:success]).not_to be_nil }
       it 'assigns the managers group' do
